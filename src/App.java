186f80e6bd53adb0c860449289cc093556647e36
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-05 08:41:31 +0300

    Finsihd task

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import University.Course;
 
 public class App {
+    public static void main(String[] args) {
+        Course firstCourse = new Course("Math", "Bob", 13);
+
+        System.out.println(firstCourse);
+    }
 }
--- src/University/Course.java
+++ src/University/Course.java
@@ -1,4 +1,50 @@
 package University;
 
 public class Course {
+
+    private String courseName;
+    private String instructor;
+    private int numberOfStudents;
+
+
+    public Course(String courseName, String instructor, int numberOfStudents) {
+
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", numberOfStudents=" + numberOfStudents +
+                '}';
+    }
 }
